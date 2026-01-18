1284b448419f7972c6fdb96140468ba4a1a4c29b
Ockddak <84654605+Ockddak@users.noreply.github.com>
2026-01-18 22:48:02 +0900

    Merge pull request #3
    * refactor: gateway keepAlive, timeout, error option add

--- gateway/src/main/java/llm/serving/gateway/proxy/api/OllamaSseProxyController.java
+++ gateway/src/main/java/llm/serving/gateway/proxy/api/OllamaSseProxyController.java
@@ -1,13 +1,19 @@
 package llm.serving.gateway.proxy.api;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
 @RestController
 public class OllamaSseProxyController {
+
+    private final Log log = LogFactory.getLog(OllamaSseProxyController.class);
 
     private final WebClient ollamaWebClient;
 
@@ -24,18 +30,37 @@ public class OllamaSseProxyController {
     )
     public Flux<String> stream() {
 
-        return ollamaWebClient.post()
+        Flux<String> ollamaStream = ollamaWebClient.post()
                 .uri("/api/generate")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue("""
-                    {
-                      "model": "llama3.2",
-                      "prompt": "Spring Cloud Gateway와 SSE를 설명해줘",
-                      "stream": true
-                    }
-                    """)
+                        {
+                          "model": "llama3.2",
+                          "prompt": "Spring Cloud Gateway와 SSE를 설명해줘",
+                          "stream": true
+                        }
+                        """)
                 // stream: 응답 시, 문자열 전체가 아닌 토큰 단위로 응답
                 .retrieve()
-                .bodyToFlux(String.class);
+                .bodyToFlux(String.class)
+                .timeout(Duration.ofSeconds(30)); //30초 동안 ollama에서 아무 데이터가 안오면 에러 발생
+
+        Flux<String> keepAlive = Flux.interval(Duration.ofSeconds(10))
+                .map(tick -> ": ping\n\n") // 클라이언트와 gateway 사이의 연결 유지를 위해 주기적으로 ping을 전달
+                .takeUntilOther(ollamaStream.ignoreElements()); //ollamaStream이 완료되는 순간 keepAlive도 같이 종료
+
+        return Flux.merge(ollamaStream, keepAlive)
+                .doOnCancel(() -> {
+                    log.info("Client disconnected - cancel Ollama request"); //클라이언트가 연결을 끊은 경우 요청 종료
+                })
+                .onErrorResume(ex -> {
+                    log.error("Ollama stream error", ex);
+
+                    String errorEvent = """
+                        event: error
+                        data: {"message":"Ollama streaming failed"}
+                        """;
+                    return Flux.just(errorEvent);
+                });
     }
 }
